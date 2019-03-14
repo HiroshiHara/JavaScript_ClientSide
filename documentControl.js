85586403fc10ed6ed021b3ed6833fdc91492850c
HaraHiroshi <mrbob403@gmail.com>
2019-03-14 21:16:05 +0900

    fuck scum SearchEngine.

--- documentControl.js
+++ documentControl.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// getAttribute/setAttribute
+// 指定した属性名の値を取得/設定できる
+// elem.getAttribute(name:属性名)/elem.setAttribute(name:属性名, value:属性値)
+function printUrlLog() {
+  var urlList = document.getElementsByTagName('a');
+  for (var i = 0; i < urlList.length; i++) {
+    var url = urlList.item(i);
+    console.log(url.getAttribute('href'));
+  }
+}
+printUrlLog();
+
+function destroyScumSearchEngine() {
+  var url = document.getElementById('scum');
+  url.setAttribute('href', 'https://www.torproject.org');
+  url.textContent = 'I did it.';
+}
+destroyScumSearchEngine();
