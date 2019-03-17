4ba2a8c200842d8cbcf647d7c0a1bfa52ee601bf
HaraHiroshi <mrbob403@gmail.com>
2019-03-17 20:09:09 +0900

    update accessFormElements.js

--- accessFormElements.js
+++ accessFormElements.js
@@ -1,9 +1,36 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('btn').addEventListener('click', function() {
+  // 入力されたテキストに応じて表示項目を書き換え
+  document.getElementById('text_btn').addEventListener('click', function() {
     var name = document.getElementById('name');
     console.log(name.value);
     var result = name.value + ' が入力されました';
     document.getElementById('name_result').textContent = result;
   }, false);
+
+  // 選択した項目に応じて表示項目を書き換え
+  document.getElementById('checkBox_btn').addEventListener('click', function() {
+    // 選択結果を格納する配列
+    var results = [];
+    // 全選択肢を格納する配列
+    var foods = document.getElementsByName('food');
+    // 全選択肢を走査し、チェック状態か否か確認
+    for (var i = 0; i < foods.length; i++) {
+      var food = foods.item(i);
+      if (food.checked) {
+        console.log(food.value);
+        results.push(food.value);
+      }
+    }
+    // チェック状態のもので表示項目を書き換え
+    var checkBox_result = '';
+    for (var i = 0; i < results.length; i++) {
+      console.log(results[i]);
+      checkBox_result += results[i] + ', ';
+    }
+    console.log(checkBox_result);
+    var sliced = checkBox_result.slice(0, -2);
+    document.getElementById('checkBox_result').textContent = sliced;
+  }, false);
+
 }, false);
