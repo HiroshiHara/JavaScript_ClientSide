7de5eca5844bc8657cc33485775dae6578f200eb
HaraHiroshi <mrbob403@gmail.com>
2019-03-21 16:00:30 +0900

    update advancedEvent

--- advancedEvent.js
+++ advancedEvent.js
@@ -1,1 +1,34 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+
+  // デフォルトの背景色
+  var defaultColor = '#86d385';
+  var first_child1 = document.getElementsByClassName('first_child1');
+
+  // mouseover/mouseoutイベントハンドラを登録する関数
+  let setMouseOverOuterEvent = function() {
+    first_child1[0].onmouseover = function() {
+      this.style.backgroundColor = '#e8ffdd';
+      this.style.transition = '.1s';
+    };
+    first_child1[0].onmouseout = function() {
+      this.style.backgroundColor = defaultColor;
+      this.style.transition = '.1s';
+    };
+  };
+
+  setMouseOverOuterEvent();
+
+  // clickイベントで上記のイベントハンドラを破棄
+  first_child1[0].onclick = function() {
+    if (!(first_child1[0].onmouseover) && !(first_child1[0].onmouseout)) {
+      setMouseOverOuterEvent();
+    } else {
+      // onxxxxに対してnullを設定する
+      first_child1[0].onmouseover = null;
+      first_child1[0].onmouseout = null;
+    }
+  };
+
+  
+}, false);
